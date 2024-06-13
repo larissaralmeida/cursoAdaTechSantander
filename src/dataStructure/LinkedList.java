37723c02b088ef0874dc65d61b5322c47050d69e
Larissa <larissaralmeida.94@gmail.com>
2024-06-13 10:50:46 -0300

    update LinkedList

--- src/dataStructure/LinkedList.java
+++ src/dataStructure/LinkedList.java
@@ -1,2 +1,25 @@
-package dataStructure;public class LinkedList {
+package dataStructure;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(String data) {
+        length = 1;
+        Node newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+        }
+    }
 }
