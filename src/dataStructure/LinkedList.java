b610fca0c328f3601022a375f438a6b494ba3353
Larissa <larissaralmeida.94@gmail.com>
2024-06-13 10:59:08 -0300

    implements methods get

--- src/dataStructure/LinkedList.java
+++ src/dataStructure/LinkedList.java
@@ -13,6 +13,45 @@ public class LinkedList {
         tail = newNode;
     }
 
+    public void getHead() {
+        if(this.head == null) {
+            System.out.println("Lista vazia!");
+        } else {
+            System.out.println("Head: " + head.data);
+        }
+
+    }
+
+
+    public void getTail() {
+        if(this.tail == null) {
+            System.out.println("Lista vazia!");
+        } else {
+            System.out.println("Tail: " + tail.data);
+        }
+
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + this.length);
+
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList("Elemento 1");
+        list.getHead();
+        list.getTail();
+        list.getLength();
+    }
+
+
 
     class Node {
         String data;
