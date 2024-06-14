d8c6232356e0157d37e5135ef213d707403d2aeb
Larissa <larissaralmeida.94@gmail.com>
2024-06-14 13:11:44 -0300

    implements top, gets and print methods

--- src/dataStructure/Stack.java
+++ src/dataStructure/Stack.java
@@ -11,6 +11,37 @@ public class Stack {
         height = 1;
     }
 
+    public void getTop() {
+        if(top == null) {
+            System.out.println("Pilha vazia!");
+        } else {
+            System.out.println("Topo da pilha: " + top.value);
+        }
+    }
+
+    public void getHeight() {
+        System.out.println("Altura da pilha: " + height);
+    }
+
+    public void print() {
+        System.out.println("---------------------");
+        Node temp = top;
+
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("---------------------");
+    }
+
+    public static void main(String[] args) {
+        Stack myStack = new Stack(4);
+
+        myStack.getTop();
+        myStack.getHeight();
+        myStack.print();
+    }
+
     class Node {
         int value;
         Node next;
